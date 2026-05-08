7dffacd846bd1a52d049b95e7bd9c0530ff49a2c
Grzegorz Kucmierz <gkucmierz@gmail.com>
2026-05-08 20:29:27 +0200

    feat: use CodeMirror for console output to sync editor themes

--- src/components/ResultCode.vue
+++ src/components/ResultCode.vue
@@ -1,40 +1,73 @@
 <script setup>
-import { ref, watch } from 'vue';
+import { ref, watch, shallowRef } from 'vue';
+import { Codemirror } from 'vue-codemirror';
+import { oneDark } from '@codemirror/theme-one-dark';
+import * as allThemes from '@uiw/codemirror-themes-all';
+import { EditorState } from '@codemirror/state';
 import settingsService from '../services/settingsService';
 
+const themesMap = {
+  oneDark,
+  dracula: allThemes.dracula,
+  githubDark: allThemes.githubDark,
+  githubLight: allThemes.githubLight,
+  nord: allThemes.nord,
+  monokai: allThemes.monokai,
+  material: allThemes.material,
+  eclipse: allThemes.eclipse
+};
+
+const currentTheme = themesMap[settingsService.getItem('theme')] || oneDark;
+const extensions = [currentTheme, EditorState.readOnly.of(true)];
+
 const props = defineProps(['data']);
-const div = ref(null);
+const view = shallowRef(null);
+
+const handleReady = payload => {
+  view.value = payload.view;
+};
 
 watch(() => props.data, () => {
   if (!settingsService.getItem('autoScroll')) return;
   setTimeout(() => {
-    if (div.value) {
-      div.value.scrollTo(0, div.value.scrollHeight);
+    if (view.value) {
+      // Scroll CodeMirror to the bottom
+      const docHeight = view.value.contentDOM.scrollHeight;
+      view.value.scrollDOM.scrollTo(0, docHeight);
     }
   }, 1);
 });
 </script>
 
 <template>
-  <div class="result-code" ref="div">
-    <pre class="selectable">{{ data }}</pre>
+  <div class="result-code">
+    <codemirror
+      :modelValue="data"
+      :style="{ height: '100%' }"
+      :extensions="extensions"
+      @ready="handleReady"
+    />
   </div>
 </template>
 
 <style lang="scss" scoped>
 .result-code {
-  overflow-x: auto;
-  overflow-y: auto;
   margin: 0;
-  padding: 8px;
   height: 100%;
   display: block;
   font-family: monospace;
-  white-space: pre;
-  line-height: normal;
+}
 
-  pre {
-    margin-top: 0;
-  }
+:deep(.cm-editor) {
+  height: 100%;
+  outline: none !important;
+}
+
+:deep(.cm-scroller) {
+  font-family: monospace;
+}
+
+:deep(.cm-gutters) {
+  display: none !important;
 }
 </style>
